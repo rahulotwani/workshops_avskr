d28c741a207ab455ffe5c7915b6818e7dd25c93e
Rahul Otwani <36023503+rahulotwani@users.noreply.github.com>
2018-09-27 00:49:43 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+$('.work-1').click(function(){
+  $('.event-description').css({'opacity': 1 , 'z-index':5});
+});
+
+
+$('.work-2').click(function(){
+  $('.event-description').css({'opacity': 1, 'z-index':5 });
+});
+
+
+$('.work-3').click(function(){
+  $('.event-description').css({'opacity': 1, 'z-index':5 });
+});
+
+$('.cross').click(function(){
+  $('.event-description').css({'opacity': 0 ,'z-index':-1});
+});
