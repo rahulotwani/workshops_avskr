a5f73cecffa6fe3bfbd56b5e017f9d4d58e9dc5c
Rahul Otwani <36023503+rahulotwani@users.noreply.github.com>
2018-09-27 17:50:16 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,6 +13,10 @@ $('.work-3').click(function(){
   $('.event-description').css({'opacity': 1, 'z-index':5 });
 });
 
+$('.work-4').click(function(){
+  $('.event-description').css({'opacity': 1, 'z-index':5 });
+});
+
 $('.cross').click(function(){
   $('.event-description').css({'opacity': 0 ,'z-index':-1});
 });
